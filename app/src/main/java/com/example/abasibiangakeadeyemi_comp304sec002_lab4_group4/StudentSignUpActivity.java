34b517d3cdd8987b197e488a6289afbf1167ff73
Abasibiangake James <jamesabasibiangake@gmail.com>
2022-07-12 00:13:58 +0100

    signup and main activity layout

---
+++ app/src/main/java/com/example/abasibiangakeadeyemi_comp304sec002_lab4_group4/StudentSignUpActivity.java
@@ -0,0 +1,14 @@
+package com.example.abasibiangakeadeyemi_comp304sec002_lab4_group4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudentSignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_sign_up);
+    }
+}
