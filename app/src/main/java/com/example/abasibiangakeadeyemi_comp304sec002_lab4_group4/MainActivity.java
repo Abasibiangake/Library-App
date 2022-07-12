fcbd85abf4bdb1be354887b832a8cb46506cb268
Abasibiangake James <jamesabasibiangake@gmail.com>
2022-07-12 02:54:33 +0100

    login layout updates

--- app/src/main/java/com/example/abasibiangakeadeyemi_comp304sec002_lab4_group4/MainActivity.java
+++ app/src/main/java/com/example/abasibiangakeadeyemi_comp304sec002_lab4_group4/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.abasibiangakeadeyemi_comp304sec002_lab4_group4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void loadLibrarianLoginScreen(View view) {
+       intent = new Intent(MainActivity.this, LibrarianLoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void loadStudentoginScreen(View view) {
+        intent = new Intent(MainActivity.this, StudentLoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void loadStudentSignUpScreen(View view) {
+        intent = new Intent(MainActivity.this, StudentSignUpActivity.class);
+        startActivity(intent);
+    }
 }
+
+
+
+// FragmentManager fragmentManager = getFragmentManager();
+//FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//        LibrarianLoginFragment librarianLoginFragment = new LibrarianLoginFragment();
+//        fragmentTransaction.replace(R.id.loadLoginFrame, librarianLoginFragment);
+//fragmentTransaction.commit();
